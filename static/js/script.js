d82fe333a75b86b11d23f73742786ed11c0c829b
yahyaitk <148454753+yahyaitk@users.noreply.github.com>
2024-08-09 13:54:10 +0200

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,80 +1,53 @@
-window.HELP_IMPROVE_VIDEOJS = false;
+class BeforeAfter {
+    constructor(enteryObject) {
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+        const beforeAfterContainer = document.querySelector(enteryObject.id);
+        const before = beforeAfterContainer.querySelector('.bal-before');
+        const beforeText = beforeAfterContainer.querySelector('.bal-beforePosition');
+        const afterText = beforeAfterContainer.querySelector('.bal-afterPosition');
+        const handle = beforeAfterContainer.querySelector('.bal-handle');
+        var widthChange = 0;
 
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
+        beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + beforeAfterContainer.offsetWidth + "px;")
+        window.onresize = function () {
+            beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + beforeAfterContainer.offsetWidth + "px;")
+        }
+        before.setAttribute('style', "width: 50%;");
+        handle.setAttribute('style', "left: 50%;");
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
+        //touch screen event listener
+        beforeAfterContainer.addEventListener("touchstart", (e) => {
 
+            beforeAfterContainer.addEventListener("touchmove", (e2) => {
+                let containerWidth = beforeAfterContainer.offsetWidth;
+                let currentPoint = e2.changedTouches[0].clientX;
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+                let startOfDiv = beforeAfterContainer.offsetLeft;
 
-    });
+                let modifiedCurrentPoint = currentPoint - startOfDiv;
 
-    var options = {
-    slidesToScroll: 1,
-    slidesToShow: 2,
-    centerMode: true, // Enable center mode
-    loop: true,
-    infinite: true,
-    autoplay: false,
-    autoplaySpeed: 3000,
-};
+                if (modifiedCurrentPoint > 10 && modifiedCurrentPoint < beforeAfterContainer.offsetWidth - 10) {
+                    let newWidth = modifiedCurrentPoint * 100 / containerWidth;
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+                    before.setAttribute('style', "width:" + newWidth + "%;");
+                    afterText.setAttribute('style', "z-index: 1;");
+                    handle.setAttribute('style', "left:" + newWidth + "%;");
+                }
+            });
+        });
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
+        //mouse move event listener
+        beforeAfterContainer.addEventListener('mousemove', (e) => {
+            let containerWidth = beforeAfterContainer.offsetWidth;
+            widthChange = e.offsetX;
+            let newWidth = widthChange * 100 / containerWidth;
+
+            if (e.offsetX > 10 && e.offsetX < beforeAfterContainer.offsetWidth - 10) {
+                before.setAttribute('style', "width:" + newWidth + "%;");
+                afterText.setAttribute('style', "z-index:" + "1;");
+                handle.setAttribute('style', "left:" + newWidth + "%;");
+            }
+        })
+
     }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
-    bulmaSlider.attach();
-
-})
-
+}
